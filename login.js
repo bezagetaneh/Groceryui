e15c72fbbfb07b536806c5baf7ebe4ca0db13585
bezagetaneh <133671222+bezagetaneh@users.noreply.github.com>
2023-05-15 17:09:44 +0300

    Add files via upload

---
+++ login.js
@@ -0,0 +1,8 @@
+function validate(){
+   const email=document.getElementById("email").Value;
+   const password=document.getElementById("password").Value;
+   if(email=="admin@gmail.com" && password=="admin"){
+    window.open= "adminpage.html";
+   }
+
+}
